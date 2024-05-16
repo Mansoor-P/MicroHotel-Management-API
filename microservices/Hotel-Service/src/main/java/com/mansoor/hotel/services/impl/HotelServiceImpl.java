09685780670eebd6288729df1c57c66e9cd05652
Mansoor-P <munaf6007@gmail.com>
2024-05-16 07:38:54 +0530

    HOTEL SERVICE

---
+++ microservices/Hotel-Service/src/main/java/com/mansoor/hotel/services/impl/HotelServiceImpl.java
@@ -0,0 +1,35 @@
+package com.mansoor.hotel.services.impl;
+
+import com.mansoor.hotel.entities.Hotel;
+import com.mansoor.hotel.exceptions.ResourceNotFoundException;
+import com.mansoor.hotel.repositories.HotelRepository;
+import com.mansoor.hotel.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Override
+    public Hotel create(Hotel hotel) {
+        String hotelId = UUID.randomUUID().toString();
+        hotel.setId(hotelId);
+        return hotelRepository.save(hotel);
+    }
+
+    @Override
+    public List<Hotel> getAll() {
+        return hotelRepository.findAll();
+    }
+
+    @Override
+    public Hotel get(String id) {
+        return hotelRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("hotel with given id not found !!"));
+    }
+}
