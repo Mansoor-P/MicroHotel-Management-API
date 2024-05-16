4acc8aa1993ec1e6fa0511adc8daa46f2ab666cf
Mansoor-P <munaf6007@gmail.com>
2024-05-16 07:40:49 +0530

    CONFIGURATION SERVER

---
+++ microservices/ConfigServer/src/test/java/com/mansoor/config/server/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mansoor.config.server.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
