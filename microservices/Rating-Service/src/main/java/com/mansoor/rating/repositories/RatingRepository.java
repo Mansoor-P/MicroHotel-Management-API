2548e46bf36f8f8a0a1e410602c7f25f7131ce56
Mansoor-P <munaf6007@gmail.com>
2024-05-16 07:39:46 +0530

    RATING SERVICE

---
+++ microservices/Rating-Service/src/main/java/com/mansoor/rating/repositories/RatingRepository.java
@@ -0,0 +1,15 @@
+package com.mansoor.rating.repositories;
+
+
+import com.mansoor.rating.entities.Rating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface RatingRepository extends MongoRepository<Rating,String>
+{
+    //custom finder methods
+    List<Rating> findByUserId(String userId);
+    List<Rating> findByHotelId(String hotelId);
+
+}
