b303c165efc886e122ebb028a56985ee4b252022
Mansoor-P <munaf6007@gmail.com>
2024-07-20 09:34:34 +0530

    project structure changed

--- microservices/Hotel-Service/src/main/java/com/mansoor/hotel/repositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mansoor.hotel.repositories;
-
-import com.mansoor.hotel.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
